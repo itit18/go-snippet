02b4870616638ce9bbd66c2281d39b2b1a995358
itit18 <itit2r@gmail.com>
2023-07-20 04:31:34 +0000

    マス目の概念とビー玉カウンタを実装

--- abc081a/main.go
+++ abc081a/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"tools"
 )
 
 func main() {
-	fmt.Println("test")
+	stdin, err := tools.FetchStdin()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(stdin)
+	cellData := makeCell(stdin[0])
+	tools.PrintStruct(cellData)
+	result := countMarble(cellData)
+
+	fmt.Println(result)
+}
+
+type cell struct {
+	values [3]bool
+}
+
+func makeCell(s string) (c cell) {
+	split_s := strings.Split(s, "")
+	for i := 0; i < len(c.values); i++ {
+		if split_s[i] == "1" {
+			c.values[i] = true
+		}
+	}
+
+	return
+}
+
+func countMarble(c cell) (count int) {
+	for i := 0; i < len(c.values); i++ {
+		if c.values[i] {
+			count++
+		}
+	}
+
+	return
 }
