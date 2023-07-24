d571a06f5900f24138b55745080bf33a21695e9e
itit18 <itit2r@gmail.com>
2023-07-24 10:06:36 +0000

    標準出力を受け取って入力値を整形する

--- abc081b/main.go
+++ abc081b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"tools"
 )
 
@@ -9,8 +10,29 @@ func main() {
 	stdin, err := tools.FetchStdin()
 	if err != nil {
 		panic(err)
-		return
 	}
 	fmt.Println(stdin)
 
+	data, err := makeAssignmentData(stdin)
+	if err != nil {
+		panic(err)
+	}
+	tools.PrintStruct(data)
+
+}
+
+// 問題文で指定された入力値の形式
+type assignmentData struct {
+	maxvalue int
+	values   []int
+}
+
+func makeAssignmentData(stdin []string) (result assignmentData, err error) {
+	result.maxvalue = tools.EasyAtoi(stdin[0])
+	sp := strings.Split(stdin[1], " ")
+	for _, d := range sp {
+		result.values = append(result.values, tools.EasyAtoi(d))
+	}
+
+	return
 }
