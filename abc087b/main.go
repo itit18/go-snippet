947055e2f7a91e193066d538af96cae40aba5de8
itit18 <itit2r@gmail.com>
2023-08-02 04:02:57 +0000

    問題文データの読み込み処理

--- abc087b/main.go
+++ abc087b/main.go
@@ -6,11 +6,37 @@ import (
 )
 
 func main() {
-	fmt.Println("aaa")
+	var output int
+
 	stdin, err := tools.FetchStdin()
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println(stdin)
+
+	input, err := makeAssignmentData(stdin)
+	if err != nil {
+		panic(err)
+	}
+	tools.PrintStruct(input)
+
+	fmt.Println(output)
+}
+
+// 問題文で定義されたデータ構造
+type assignmentData struct {
+	coinOf500yen int
+	coinOf100yen int
+	coinOf50yen  int
+	totalAmount  int
+}
+
+// 標準入力を問題文データに整形
+func makeAssignmentData(stdin []string) (data assignmentData, err error) {
+	data.coinOf500yen = tools.EasyAtoi(stdin[0])
+	data.coinOf100yen = tools.EasyAtoi(stdin[1])
+	data.coinOf50yen = tools.EasyAtoi(stdin[2])
+	data.totalAmount = tools.EasyAtoi(stdin[3])
+
+	return
 }
